20374cc0cabb72168d925a5d554382443a17b009
Olly Betts <olly@survex.com>
2020-07-30 19:11:16 +1200

    Adjust to work with older zlib versions
    Use gzread() instead of gzfread() (which was added in 1.2.9) - the code
    is actually slightly simpler this way too.

--- src/P98_gzip.h
+++ src/P98_gzip.h
@@ -4,7 +4,7 @@ inline int decompress_bundle(const char* path,
 			     void* buf,
 			     int buflen) {
     gzFile in = gzopen(path, "rb");
-    int result = gzfread((voidp)buf, 1, buflen, in);
+    int result = gzread(in, (voidp)buf, buflen);
     gzclose(in);
     return result;
 }
