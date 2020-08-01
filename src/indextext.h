545a3d680071f3973197d918f1d1c0b7d22e8ba5
Olly Betts <olly@survex.com>
2020-08-01 12:18:59 +1200

    Remove index-xapian-trec
    It seems to broadly duplicate the functionality of trec_index, but
    the code is crustier, and it's marked as "Internal use only: for xapian
    paper experiments" in a comment at the top.
    
    It wasn't actually even being built until a commit I made earlier today.

--- src/indextext.h
+++ src/indextext.h
@@ -25,21 +25,11 @@
 #ifndef _INDEXTEXT_H_
 #define _INDEXTEXT_H_
 
-#include <xapian.h>
-
-#include <limits.h>
 #include <ctype.h>
-
-#include "symboltab.h"
-
-using std::string;
-
-// Put a limit on the size of terms to help prevent the index being bloated
-// by useless junk terms
-static const unsigned int MAX_PROB_TERM_LENGTH = 64;
+#include <string>
 
 static inline void
-lowercase_term(string &term)
+lowercase_term(std::string &term)
 {
     string::iterator i = term.begin();
     while (i != term.end()) {
@@ -48,84 +38,4 @@ lowercase_term(string &term)
     }
 }
 
-class AccentNormalisingItor {
-  private:
-    string::const_iterator itor;
-    char queued;
-
-  public:
-    AccentNormalisingItor()
-	: itor(), queued(0) {}
-    AccentNormalisingItor(string::const_iterator itor_)
-	: itor(itor_), queued(0) {}
-    void operator=(string::const_iterator itor_)
-    {
-	itor = itor_;
-	queued = 0;
-    }
-    bool operator==(const AccentNormalisingItor &o) const {
-	return queued == o.queued && itor == o.itor;
-    }
-    bool operator!=(const AccentNormalisingItor &o) const {
-	return !(*this == o);
-    }
-    char operator*() const {
-	if (queued) return queued;
-	unsigned char ch = (unsigned char)*itor;
-	if (ch >= 160
-#if CHAR_BIT > 8
-		      && ch < 256
 #endif
-				 ) return TRANSLIT1[ch - 160];
-	return (char)ch;
-    }
-    AccentNormalisingItor & operator++() {
-	this->operator++(0);
-	return *this;
-    }
-    void operator++(int) {
-	if (queued) {
-	    queued = 0;
-	} else {
-	    unsigned char ch = (unsigned char)*itor;
-	    if (ch >= 160
-#if CHAR_BIT > 8
-			  && ch < 256
-#endif
-				     ) {
-		ch = TRANSLIT2[(unsigned char)ch - 160];
-		if (ch != ' ') {
-		    queued = ch;
-		    return;
-		}
-	    }
-	}
-	++itor;
-    }
-    string::const_iterator raw() const { return itor; }
-    /// Allow use as an STL iterator
-    //@{
-    typedef std::input_iterator_tag iterator_category;
-    typedef char value_type;
-    typedef string::size_type difference_type;
-    typedef const char * pointer;
-    typedef const char & reference;
-    //@}
-};
-
-Xapian::termpos
-index_text(const std::string &s, Xapian::Document &doc, Xapian::Stem &stemmer,
-	   Xapian::termcount wdfinc, const std::string &prefix,
-	   Xapian::termpos pos = static_cast<Xapian::termpos>(-1)
-	   // Not in GCC 2.95.2 numeric_limits<Xapian::termpos>::max()
-	   );
-
-inline Xapian::termpos
-index_text(const std::string &s, Xapian::Document &doc, Xapian::Stem &stemmer,
-	   Xapian::termpos pos)
-{
-    return index_text(s, doc, stemmer, 1, std::string(), pos);
-}
-
-#endif
-
